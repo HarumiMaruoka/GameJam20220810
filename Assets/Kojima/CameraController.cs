ad82db4a0e124dad99408af7139552ae39ad0745
kojimayuta4649753 <getaway5356@gmail.com>
2022-08-10 14:30:33 +0900

    カメラがゴール地点でスクロールを実行しないようにしました

--- Assets/Kojima/CameraController.cs
+++ Assets/Kojima/CameraController.cs
@@ -11,11 +11,22 @@ public class CameraController : MonoBehaviour
     //カメラのスタート位置
     private float startPosX;
     //カメラのゴール位置
+    [SerializeField]
     private float endPosX;
+    //カメラのゴール地点での判定
+    bool isCamJudge = false;
 
     void Update()
     {
-        transform.position = new Vector3(transform.position.x + (scrollSpeed * Time.deltaTime), 0, transform.position.z);
+        if (transform.position.x <= endPosX)
+        {
+            transform.position = new Vector3(transform.position.x + (scrollSpeed * Time.deltaTime), 0, transform.position.z);
+        }
+        else if(transform.position.x >= endPosX && !isCamJudge)
+        {
+            transform.position = new Vector3(endPosX, 0, transform.position.z);
+            isCamJudge = true;
+        }
         Debug.Log(transform.position);
     }
 }
