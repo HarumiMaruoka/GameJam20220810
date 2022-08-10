0c569ea56f3dc61790c89fef7380f4a6c1ef72b4
Ryuto034 <ryukyu.river0309@gmail.com>
2022-08-10 13:36:43 +0900

    プレイヤーの動き

--- Assets/Maruoka/PlayerMove.cs
+++ Assets/Maruoka/PlayerMove.cs
@@ -6,9 +6,21 @@ public class PlayerMove : MonoBehaviour
 {
     //*************** メンバー変数 ***************//
     Rigidbody2D _rigidbody2D;
-    Vector3 _newVelocity;
+    Vector2 _newForce;
+    Vector2 _newVelocity;
     float _inputX;
     float _inputY;
+
+    [SerializeField]
+    float upForce = 3.0f;
+    [SerializeField]
+    float sideSpeed = 2.0f;
+
+
+    private void Start()
+    {
+        Initialized();
+    }
 
     void Update()
     {
@@ -27,12 +39,28 @@ public class PlayerMove : MonoBehaviour
     /// <summary> 入力 </summary>
     private void Input_PlayerMove()
     {
+        _inputY = Input.GetKey(KeyCode.W) ? 1 : 0;  //上昇
+        _inputX = Input.GetAxis("Horizontal");      //左右移動
     }
 
     /// <summary> 更新 </summary>
     private void Update_PlayerMove()
     {
         //リセット
-        _newVelocity = Vector3.zero;
+        _newVelocity = Vector2.zero;
+        _newForce = Vector2.zero;
+
+        //入力値から計算
+        _newVelocity = new Vector2(_inputX * sideSpeed, 0);
+        _newVelocity.y = _rigidbody2D.velocity.y;
+        _newForce = new Vector2(0, _inputY * upForce);
+
+        //X方向velocity、Y方向AddForceで更新
+        _rigidbody2D.velocity = _newVelocity;
+        _rigidbody2D.AddForce(_newForce);
+
+        //velocityのパターン
+        //if(_inputY == 0) _newVelocity.y = _rigidbody2D.velocity.y;
+        //_rigidbody2D.velocity = _newVelocity;
     }
 }
