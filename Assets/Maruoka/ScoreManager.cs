484f8322b002d87169f729fc1804abf9d1ef9ce0
kojimayuta4649753 <getaway5356@gmail.com>
2022-08-10 18:00:27 +0900

    ステージ中のスコアの表示を作成しました

--- Assets/Maruoka/ScoreManager.cs
+++ Assets/Maruoka/ScoreManager.cs
@@ -1,9 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary> ただのスコアマネージャー </summary>
 public class ScoreManager : MonoBehaviour
 {
+    public GameObject scoreObj= null;
     static public int _score = 0;
+
+    void Update()
+    {
+       Text scoreText = scoreObj.GetComponent<Text>();
+        scoreText.text = "SCORE:" + _score;
+    }
 }
