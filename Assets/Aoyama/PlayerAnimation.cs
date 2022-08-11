60571137dfe0e0b20ce462b333ce0b568168b27e
kent\vantan <atoshi1129@gmail.com>
2022-08-11 12:30:16 +0900

    頼むぞ...

--- Assets/Aoyama/PlayerAnimation.cs
+++ Assets/Aoyama/PlayerAnimation.cs
@@ -6,6 +6,7 @@ public class PlayerAnimation : MonoBehaviour
 {
     Rigidbody2D _rb;
     SpriteRenderer _sr;
+    ParticleSystem _ps;
 
     [Header("レベルが変化するスコア数"), SerializeField] int _levelUpScore = 10;
     [Header("Animationが変わる上方向のスピード1"), SerializeField] float _changeUp1 = -0.8f;
@@ -25,6 +26,7 @@ public class PlayerAnimation : MonoBehaviour
     {
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
+        _ps = GetComponent<ParticleSystem>();
     }
 
 
@@ -34,17 +36,22 @@ public class PlayerAnimation : MonoBehaviour
 
         if (level > 1)
         {
-            
             AnimationControl(_level3);
         }
         else if (level > 0)
-        {
-
+        {           
             AnimationControl(_level2);
         }
         else
         {
             AnimationControl(_level1);
+        }
+
+
+        if (!_isChange)
+        {
+            _isChange = true;
+            _ps.Play();
         }
     }
 
